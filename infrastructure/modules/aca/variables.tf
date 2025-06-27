d3a13a36629e1c91be95ef1c05aea12b8ec9d83d
Mdumisi Kelvin Letsie <mdu.letsie7@gmail.com>
2025-06-27 04:28:58 +0000

    Apply common tags to Azure resources created and managed by  Terraform.

--- infrastructure/modules/aca/variables.tf
+++ infrastructure/modules/aca/variables.tf
@@ -1,10 +1,26 @@
-variable "container_app_name" {}
-variable "resource_group_name" {}
-variable "location" {}
-variable "acr_login_server" {}
-variable "acr_id" {}
+variable "container_app_name" {
+  description = "Name of the contaier app."
+  type        = string
+}
+variable "resource_group_name" {
+  description = "Name of the resource group."
+  type        = string
+}
+variable "location" {
+  description = "Resource group location."
+  type        = string
+}
+variable "acr_login_server" {
+  description = "Azure container registry login server."
+  type        = string
+}
+variable "acr_id" {
+  description = "Azure container registry ID."
+  type        = string
+}
 variable "image_tag" {
-  default = "latest"
+  description = "Image tag of image to deploy to Azure Container App."
+  default     = "latest"
 }
 
 variable "common_tags" {
