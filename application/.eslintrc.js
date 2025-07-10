7b6e70fd2fcbcd9c4053091469f4c1e9ed477b71
Mdumisi Kelvin Letsie <mdumisidev@gmail.com>
2025-07-10 22:15:04 +0200

    Refactor ESLint configuration and update linting command in CI/CD pipeline

--- application/.eslintrc.js
+++ application/.eslintrc.js
@@ -3,9 +3,17 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true
   },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  rules: {},
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {}
 };
