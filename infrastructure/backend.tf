b653ed34502f617ff074fd842a563b7bd6e2126b
Mdumisi Kelvin Letsie <mdu.letsie7@gmail.com>
2025-09-15 21:43:50 +0200

    Begin IaC move to Terragrunt

--- infrastructure/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfstatekrypticinfra"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
